7eb586c5d82282b53b0d49948cfdccc5cab723f7
Abel <Abel15655@gmail.com>
2020-09-04 14:40:24 +0100

    Updates

---
+++ precache-manifest.e5c24f832cf8af395829c651908df7e9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6660a99f9129bec8a23f1f2cc0d22d86",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "967812043e6ba82ffa31",
+    "url": "/todo-app/static/css/main.3321a4f5.chunk.css"
+  },
+  {
+    "revision": "d8f0aca736dd855584ed",
+    "url": "/todo-app/static/js/2.ca22cfb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.ca22cfb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "967812043e6ba82ffa31",
+    "url": "/todo-app/static/js/main.a54746d1.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/todo-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/todo-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/todo-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/todo-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/todo-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
